6ba3cd20a56363067a6f37ea5d0a59807f2f28a9
Juan Pablo Correa Puerta <jp.correap@uniandes.edu.co>
2020-04-08 12:07:22 -0500

    Data endpoint para las actividades creado y probado

--- db/MongoUtils.js
+++ db/MongoUtils.js
@@ -3,6 +3,7 @@ const ObjectID = require("mongodb").ObjectID;
 require("dotenv").config();
 
 const url = process.env.DB_URL;
+const dbName = "quedateEnCasa";
 
 function MongoUtils() {
   const mu = {};
@@ -15,29 +16,73 @@ function MongoUtils() {
     return client.connect();
   };
 
+  // -----------------------
+  // Activities operations
+  // -----------------------
+  mu.actividades = {};
+
+  // Get all global activities on database
+  mu.actividades.getAll = () =>
+    mu.connect().then((client) =>
+      client
+        .db(dbName)
+        .collection("actividades")
+        .find({})
+        .toArray()
+        .finally(() => client.close())
+    );
+
+  // ESTA PENDIENTE
+  // Get all personal activities of an specific user
+  mu.actividades.getByUserID = (userID) =>
+    mu.connect().then((client) =>
+      client
+        .db(dbName)
+        .collection("actividades")
+        .find({ _id: userID })
+        .toArray()
+        .finally(() => client.close())
+    );
+
+  // ESTA PENDIENTE
+  // Inserts one personal activitie on database
+  mu.actividades.insert = (query) =>
+    mu.connect().then((client) =>
+      client
+        .db(dbName)
+        .collection("usuarios")
+        .updateOne(query)
+        .finally(() => client.close())
+    );
+
   // ----------------------
   // Users operations
   // ----------------------
   mu.users = {};
 
-  //creates a new user of the application
+  // Create a new user of the application
   mu.users.create = (username, password) =>
     mu.connect().then((client) => {
-      console.log("Se conectó a la base de datos y va a guardar ", username, ":", password );
-      const usuarios = client.db("quedateEnCasa").collection("usuarios");
+      console.log(
+        "Se conectó a la base de datos y va a guardar ",
+        username,
+        ":",
+        password
+      );
+      const usuarios = client.db(dbName).collection("usuarios");
 
       return usuarios
-        .insertOne({ "username": username, "password": password })
+        .insertOne({ username: username, password: password })
         .finally(() => client.close());
     });
 
   // Get a specific user by username
   mu.users.findByUsername = (user, cb) =>
     mu.connect().then((client) => {
-      const usuarios = client.db("quedateEnCasa").collection("usuarios");
+      const usuarios = client.db(dbName).collection("usuarios");
 
       return usuarios
-        .findOne({ "username": user })
+        .findOne({ username: user })
         .finally(() => client.close())
         .then((user) => {
           console.log("Encontró al usuario ", user);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,49 @@
 const express = require("express");
 const router = express.Router();
-//const bd = require("../db/MongoUtils.js");
-
+const bd = require("../db/MongoUtils.js");
+const bu = require("../db/BcryptUtils.js");
 
 router.get("/", function (req, res) {
   res.render("home", { user: req.user });
 });
 
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  try {
+    bd.users.findByUsername(req.body.name, (nada, user) => {
+      console.log("Llegó el usuario ", user);
+      if (user == null) {
+        let hashedPassword = bu.Accounts.generateHash(req.body.password);
+        bd.users
+          .create(req.body.name, hashedPassword)
+          .then(res.redirect("/login"));
+      } else {
+        console.log(req.body.name, "already exists!");
+        res.redirect("/register");
+      }
+    });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 // Data endpoints
+
+// Get all global activities
+router.get("/activities", (req, res) => {
+  bd.actividades.getAll().then((activities) => {
+    res.json(activities);
+  });
+});
+
+// Get personal activities created by an specific user
+router.post("/:userID/activities", (req, res) => {
+  bd.actividades.getByUserID(req.params.userID).then((activities) => {
+    res.json(activities);
+  });
+});
 
 module.exports = router;
